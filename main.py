28af618b98590b68bde90883792ebcf20d7384b7
ProjectMAI <rodionov.mihail02@mail.ru>
2021-09-07 19:50:04 +0300

    ver2

--- main.py
+++ main.py
@@ -1,21 +1,62 @@
-from kivy.uix.screenmanager import Screen
+from kivy.app import App
+from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.anchorlayout import AnchorLayout
+from kivy.core.window import Window
 
-from kivymd.app import MDApp
-from kivymd.uix.button import MDRectangleFlatButton
+Window.clearcolor = (1, 1, 1, 1)    # установка цвета фона окна
 
-print("Oke?")
-
-
-
-class MainApp(MDApp):
+class BoxApp(App):
     def build(self):
-        screen = Screen()
-        screen.add_widget(
-            MDRectangleFlatButton(
-                text="Hello, World",
-                pos_hint={"center_x": 0.5, "center_y": 0.5},
-            )
-        )
-        return screen
+        al = AnchorLayout()
+        bl = BoxLayout(orientation = 'vertical', size_hint = [.5, .5])
 
-MainApp().run()
+        self.text_login = TextInput()
+        self.text_password = TextInput()
+
+        bl.add_widget(self.text_login)  # позиция Логин
+        bl.add_widget(self.text_password) # позиция Пароль
+
+        # Настройка кнопки  Войти
+        bl.add_widget(Button(
+            text= "Войти",
+
+            font_size = 13,  # font_size рaзмер шрифта
+            on_press=self.login_press,  # on_press нажата
+            background_color=[1, 0, 0, 1],  # background_color цвет RGBA в %
+            background_normal="",  # background_normal  делает цвет ярче
+                     ))
+
+        # Настройка кнопки  Зарегестрироваться
+        bl.add_widget(Button(
+            text = "Зарегестрироваться",
+            font_size=13,  # font_size рaзмер шрифта
+            on_press=self.password_press,  # on_press нажата
+            background_color=[.5, .19, .88, 1],  # background_color цвет RGBA в %
+            background_normal="",  # background_normal  делает цвет ярче
+                    ))
+
+
+        al.add_widget(bl)
+        return al
+
+    def on_text(self,instance,value):
+        self.on_text.text = print(self.on_text)
+
+
+
+ # Вывод пароля и логина в консоль
+
+    # При клике на "Войти"
+    def login_press(self, instance):
+        print(self.text_login.text)
+        print(self.text_password.text)
+
+    # При клике на "Зарег."
+    def password_press(self, instance):
+        print(self.text_login.text)
+        print(self.text_password.text)
+
+if __name__ == "__main__":
+    BoxApp().run()
