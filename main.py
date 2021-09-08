96fe632d8c66670e6e8479bacf58b9eff2ad14c7
ProjectMAI <rodionov.mihail02@mail.ru>
2021-09-08 23:08:55 +0300

    Ver3=UserRegistrationIsDone

--- main.py
+++ main.py
@@ -5,7 +5,22 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.anchorlayout import AnchorLayout
 from kivy.core.window import Window
 
+import sqlite3
+
+db = sqlite3.connect('server.base') # создаем таблицу
+sql = db.cursor() # Он позволяет делать SQL-запросы к базе
+
+
+#подкл. к табл. и создаем 2 столбца - login и password
+sql.execute("""CREATE TABLE IF NOT EXISTS users (
+    login TEXT,
+    password TEXT
+) """)
+db.commit()
+
+
 Window.clearcolor = (1, 1, 1, 1)    # установка цвета фона окна
+
 
 class BoxApp(App):
     def build(self):
@@ -50,13 +65,43 @@ class BoxApp(App):
 
     # При клике на "Войти"
     def login_press(self, instance):
-        print(self.text_login.text)
-        print(self.text_password.text)
+        user_login = self.text_login.text
+        user_password = self.text_password.text
+
+
+        sql.execute(f"SELECT Login FROM users WHERE login = '{user_login}' ")
+        if sql.fetchone() is None:
+            print('Такого пользователя нет! Зарегистрируйтесь! Укажите почту и пароль, и нажмите "Зарегестрироваться"')
+            #for value in sql.execute(" SELECT * FROM users "):  # вывод таблицы
+            #    print(value)  # вывод таблицы
+
+        else:
+            sql.execute(f"SELECT Password FROM users WHERE Password = '{user_password}' ")
+            if sql.fetchone() is None:
+                print('Неверный пароль! Попробуйте ещё раз!')
+
+            else:
+                print('Успешно!')
+
+
+
+
+
 
     # При клике на "Зарег."
     def password_press(self, instance):
-        print(self.text_login.text)
-        print(self.text_password.text)
+        user_login = self.text_login.text
+        user_password = self.text_password.text
+
+
+        sql.execute(f"SELECT Login FROM users WHERE login = '{user_login}' ")
+        if sql.fetchone() is None:
+            sql.execute(f"INSERT INTO users VALUES (?, ?)", (user_login, user_password))
+            db.commit()
+            print('Вы успешно зарегистрировались! Введите свои данные и нажмите "Войти"')
+        else:
+            print('Такой пользователь уже есть! Попробуйте войти')
+
 
 if __name__ == "__main__":
     BoxApp().run()
